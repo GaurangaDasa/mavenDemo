183a090c95674de09e49185e9a2d14b396cebb3c
user7 <kuldeepd@cybage.com>
2015-01-04 14:01:31 +0530

    adding all

---
+++ src/main/java/test/cybage/SampleDemoMaven/App.java
@@ -0,0 +1,14 @@
+package test.cybage.SampleDemoMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is core maven sample");
+    }
+}
